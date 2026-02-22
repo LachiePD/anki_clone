654c7d48a323e0082df184b1927e5a495bc0da5c
lachie <lachiepd@gmail.com>
2026-02-22 15:45:19 +1100

    changed font for the app

--- anki-clone/app/homescreen/page.js
+++ anki-clone/app/homescreen/page.js
@@ -1,5 +1,6 @@
 "use client";
 import { useDeckList } from "./DeckListContext.jsx";
+import Card from "@/components/Card.jsx";
 import SideBar from "./_components/Sidebar.jsx";
 import Creator from "./_components/Creator.jsx";
 import Controller from "./Views/Controller.jsx";
@@ -7,13 +8,20 @@ import Controller from "./Views/Controller.jsx";
 const page = () => {
   const deckList = useDeckList();
   return (
-    <div className={"flex w-full   "}>
+    <main className={"flex w-full   "}>
       <SideBar />
-      <div className={" flex-1     p-5 bg-primary "}>
-        <h1 className={"text-3xl"}>my sticky brain :)</h1>
-        {deckList.selectedDeck ? <Controller /> : <Creator />}
-      </div>
-    </div>
+      <section className={" flex flex-1 flex-col min-h-screen p-5 bg-primary "}>
+        //TODO , make a directive for section headers
+        <header>
+          <h1 className={"text-3xl "}>my sticky brain :)</h1>
+        </header>
+        <div
+          className={"flex-1 flex items-center justify-center  "}
+        >
+          <Card>{deckList.selectedDeck ? <Controller /> : <Creator />}</Card>
+        </div>
+      </section>
+    </main>
   );
 };
 
