bbe2d82d2011293c234dc83dee3269f0f544dd24
lachie <lachiepd@gmail.com>
2026-02-22 18:15:56 +1100

    updated <Card/> css, moved its css into its own file instead of globals.css

--- anki-clone/components/Card.jsx
+++ anki-clone/components/Card.jsx
@@ -1,7 +1,7 @@
 
 const Card = ({ header, children }) => {
   return (
-    <div className={"card w-fit"}>
+    <div className={"bg-primary shadow-xl border border-secondary rounded-2xl flex-1 max-w-4xl"}>
       <h1 className={"text-2xl "}>{header}</h1>
       {children}
     </div>
