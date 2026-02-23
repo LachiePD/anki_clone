6ddc4f8d9faa6d6f0b6c624dc0b1e2b6f37ad988
lachie <lachiepd@gmail.com>
2026-02-23 19:59:45 +1100

    refactored

--- front-end/app/login/LoginForm.jsx
+++ front-end/app/login/LoginForm.jsx
@@ -1,62 +1,49 @@
-
 import { attemptLogin } from "@/api/auth/auth.js";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
-
 const LoginForm = () => {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
+  const [formData, setFormData] = useState({ userName: "", password: "" });
   const router = useRouter();
 
-
-  const handleChange = (e, credential) => {
-    switch (credential) {
-      case "userName":
-        setUserName(e.target.value);
-        break;
-      case "password":
-        setPassword(e.target.value);
-        break;
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const result = await attemptLogin({ userName, password });
+    const result = await attemptLogin({userName:formData.userName, password:formData.password});
 
     if (result.status !== 200) {
-	    throw new Error
+      throw new Error();
       return;
     }
     router.push("/homescreen");
   };
 
   return (
-    <form className={" flex mx-auto gap-4 flex-col "} onSubmit={(e) => handleSubmit(e)}>
+    <form
+      className={" flex mx-auto gap-4 flex-col "}
+      onSubmit={handleSubmit}
+    >
       <label>Username: </label>
       <input
+        name="userName"
         type="text"
         className={"border-2"}
-        onChange={(e) => {
-          handleChange(e, "userName");
-        }}
+        onChange={handleChange}
       />
-      <br />
 
       <label>Password: </label>
       <input
+        name="password"
         type="password"
         className={"border-2"}
-        onChange={(e) => {
-          handleChange(e, "password");
-        }}
+        onChange={handleChange}
       />
-      <br />
-      <button
-        type={"submit"}
-	  className={'button'}
-      >
+
+      <button type={"submit"} className={"button"}>
         Login
       </button>
     </form>
