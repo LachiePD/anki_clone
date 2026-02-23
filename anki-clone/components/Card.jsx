3b8e542d4ef9e138d5ac7b67a7eba355b8b1a5d5
lachie <lachiepd@gmail.com>
2026-02-23 16:44:36 +1100

    changed sizing login on card

--- anki-clone/components/Card.jsx
+++ anki-clone/components/Card.jsx
@@ -1,15 +1,15 @@
 const styles = {
   card: `
-	bg-primary shadow-xl rounded-2xl flex-1  flex flex-col
+	bg-primary shadow-xl rounded-2xl   flex flex-col
 	border border-secondary 
 	items-center
 	justify-center
 	`,
 };
 
-const Card = ({ header, children }) => {
+const Card = ({ header, children, className }) => {
   return (
-    <div className={`${styles.card}`}>
+    <div className={`${styles.card} ${className}`}>
       <h1 className={"text-2xl "}>{header}</h1>
       {children}
     </div>
