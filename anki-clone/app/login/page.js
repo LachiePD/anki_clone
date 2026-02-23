1e0571d3afec4856a99f082557dbab65ebd207f7
lachie <lachiepd@gmail.com>
2026-02-23 16:55:59 +1100

    made the layout of <Card> better built

--- anki-clone/app/login/page.js
+++ anki-clone/app/login/page.js
@@ -4,8 +4,8 @@ import LoginForm from "@/app/login/LoginForm.jsx";
 
 const Page = () => {
   return (
-    <main className={"flex p-20 mx-auto"}>
-      <Card header={"LOGIN"} className={"flex-1 max-w-4xl"}>
+    <main className={"flex w-full min-h-screen items-center justify-center"}>
+      <Card header={"LOGIN"} >
         <LoginForm />
       </Card>
     </main>
