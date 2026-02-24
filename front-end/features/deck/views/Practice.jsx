0fad65a100e9fc16fa8cc802c763fa9b18d74816
lachie <lachiepd@gmail.com>
2026-02-24 18:42:20 +1100

    making Practice dumber, will make <Flashcard/> decide on what to show, and then the ActiveDeck will decide on whether or not its finished

--- front-end/features/deck/views/Practice.jsx
+++ front-end/features/deck/views/Practice.jsx
@@ -1,22 +1,6 @@
-import { Flashcard, useFlashcard } from "@/features/flashcard/index.js";
-import {useActiveDeck} from '../providers/ActiveDeckProvider';
+import { Flashcard } from "@/features/flashcard/index.js";
 //TODO Practice shouldnt want to use useFlashcard
 export const Practice = () => {
-  const activeDeck = useActiveDeck();
-  const flashcardService = useFlashcard({cardList: activeDeck.cardList});
 
-  const chooseOutput = () => {
-    if (flashcardService.isFinished) {
-      return <div>DeckFinished!</div>;
-    } else {
-      return (
-        <Flashcard
-          isRevealed={flashcardService.isRevealed}
-          content={flashcardService.content}
-          actions={flashcardService.actions}
-        />
-      );
-    }
-  };
-  return <>{chooseOutput()}</>;
+  return <><Flashcard/><>;
 };
