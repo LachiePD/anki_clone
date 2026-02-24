ff8945e937162b2c5c702cc28d87b69ea1f5b4dd
lachie <lachiepd@gmail.com>
2026-02-24 18:35:57 +1100

    making useFlashcard dumber, so now it will just call functions from ActiveDeck Provider, rather than having logic of its own

--- front-end/features/flashcard/useFlashcard.jsx
+++ front-end/features/flashcard/useFlashcard.jsx
@@ -1,31 +1,18 @@
 import { useState, useEffect } from "react";
-import {useDeckList} from '@/features/deck/index.js'
-export const useFlashcard = ({ cardList = [] }) => {
-  const [activeCardIndex, setActiveCardIndex] = useState(0);
+import { useActiveDeck } from "@/features/deck/index.js";
+
+export const useFlashcard = () => {
+  const activeDeck = useActiveDeck();
   const [content, setContent] = useState({ front: "", back: "" });
   const [isRevealed, setRevealed] = useState(false);
 
-  const isFinished = activeCardIndex >= cardList.length;
-
   useEffect(() => {
     getNewCard(activeCardIndex);
-  }, [activeCardIndex, cardList]);
-
-  const nextCard = () => {
-    setActiveCardIndex((prev) => prev + 1);
-  };
+  }, []);
 
   const toggleRevealed = () => {
     setRevealed((prev) => !prev);
   };
-  const getNewCard = () => {
-    if (activeCardIndex + 1 > cardList.length) {
-      return;
-    }
-    const newCard = cardList[activeCardIndex];
-    setContent({ front: newCard.front, back: newCard.back });
-    setRevealed(false);
-  };
 
-  return { content, isRevealed, isFinished, actions: { nextCard, toggleRevealed } };
+  return {};
 };
