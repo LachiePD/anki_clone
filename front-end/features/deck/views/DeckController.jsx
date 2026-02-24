0af7458e9a1d01046cdb73748736f89ab4e579ca
lachie <lachiepd@gmail.com>
2026-02-24 20:16:52 +1100

    removing unused imports

--- front-end/features/deck/views/DeckController.jsx
+++ front-end/features/deck/views/DeckController.jsx
@@ -1,13 +1,10 @@
 "use client";
-import { useDeckMode } from "../hooks/useDeckMode.jsx";
 import { useActiveDeck } from "../providers/ActiveDeckProvider.jsx";
-import { useDeckList } from "../providers/DeckListContext.jsx";
 import { Practice } from "./Practice.jsx";
 import { Inspect } from "./Inspect.jsx";
 
 export const DeckController = () => {
   const activeDeck = useActiveDeck();
-  const mode = activeDeck.mode;
 
   const renderMode = () => {
     switch (mode.currentMode) {
