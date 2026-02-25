9325483278a9645966c62f515e647d68f6e71b1b
lachie <lachiepd@gmail.com>
2026-02-26 03:29:30 +1100

    useApi/ implementation v2

--- front-end/features/api/useApi.jsx
+++ front-end/features/api/useApi.jsx
@@ -4,14 +4,24 @@ import { card } from "./card.api.js";
 import { deck } from "./deck.api.js";
 
 export const useApi = () => {
-  const [response, setResponse] = useState(null);
+  const { token, logout } = useAuth();
+  //TODO deal with expired jwt here
+  const wrap =
+    (func) =>
+    async (...args) => {
+      const data = await func(...args, token);
+      if (data?.status === 401) logout();
+      return data;
+    };
 
-  const createUser = async (credentials) => {
-    await auth.createUser(credentials);
+  return {
+    auth: {
+      login: wrap(auth.login),
+      create: wrap(auth.createUser),
+    },
+    cards: {
+      fetch: wrap(card.fetchByDeck),
+      create: wrap(card.createNewCard),
+    },
   };
-
-  const attemptLogin = async (credentials) => {
-    await auth.attemptLogin(credentials);
-  };
-  return { createUser };
 };
