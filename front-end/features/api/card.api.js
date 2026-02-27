9f5e7b685c0d0b0619d745a10b33687617285255
lachie <lachiepd@gmail.com>
2026-02-27 16:52:58 +1100

    refactored

--- front-end/features/api/card.api.js
+++ front-end/features/api/card.api.js
@@ -1,32 +1,16 @@
+import { request } from "./request.js";
+
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-export const fetchByDeck = async (id) => {
-  const response = await fetch(`${apiUrl}/fetchCardsForDeck?id=${id}`, {
+const fetchByDeck = (queryParam) =>
+  request(`${apiUrl}/fetchCardsForDeck?id=${queryParam}`, {
     method: "GET",
-    credentials: "include",
-    headers: { "Content-Type": "application/json" },
   });
 
-  const data = await response.json();
-  return data;
-};
-
-export const createNewCard = async (deckId, card) => {
-  const response = await fetch(`${apiUrl}/createNewCard`, {
+const newCard = (payload) =>
+  request(`${apiUrl}/createNewCard`, {
     method: "POST",
-    credentials: "include",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ deckId, card }),
+    body: JSON.stringify(payload),
   });
 
-  const data = await response.json();
-
-  if (!response.ok) {
-    return {
-      error: data.error || "Something went wrong in createNewCard api call",
-    };
-  }
-  return data;
-};
-
-export const card = { createNewCard, fetchByDeck };
+export const card = { newCard, fetchByDeck };
