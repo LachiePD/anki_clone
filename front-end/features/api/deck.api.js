0cb89c0875e705b2769839a317dd7c17f85f2fe0
lachie <lachiepd@gmail.com>
2026-02-27 16:29:25 +1100

    simplified, refactored

--- front-end/features/api/deck.api.js
+++ front-end/features/api/deck.api.js
@@ -1,32 +1,23 @@
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-const routeFactory =
-  ({ route, method }) =>
-  async (payload) => {
-    const options = {
-      headers: { "Content-Type": "application/json" },
-      credentials: "include",
-      method,
-    };
+async function request(path, options = {}) {
+  const response = await fetch(`${apiUrl}${path}`, {
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+    ...options, // This lets us overwrite or add method/body
+  });
 
-    if (method !== "GET" && payload) {
-      options.body = JSON.stringify(payload);
-    }
-    const response = await fetch(route, options);
-    const data = await response.json();
-    return { ...data, status: response.status };
-  };
+  const data = await response.json();
+  return { ...data, status: response.status };
+}
+export const getDecks = () => request("/getAllDecks", { method: "GET" });
 
-export const getDecks = routeFactory({
-  route: `${apiUrl}/getAllDecks`,
-  method: "GET",
-});
-export const createDeck = routeFactory({
-  route: `${apiUrl}/createdeck`,
-  method: "GET",
-});
+export const removeDeck = (id) =>
+  request(`/removeDeck/${encodeURIComponent(id)}`, { method: "DELETE" });
 
-export const removeDeck = routeFactory({
-  route: `${apiUrl}/removeDeck/${encodeURIComponent(deckId)}`,
-  method: "DELETE",
-});
+export const createDeck = (payload) =>
+  request("/createDeck", {
+    method: "POST",
+    body: JSON.stringify(payload),
+  });
+export const deck = { getDecks, createDeck, removeDeck };
