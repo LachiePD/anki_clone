01c1c387d4ea725129eda26b09282fbc39bc70c4
lachie <lachiepd@gmail.com>
2026-04-01 01:10:43 +1100

    refactor: phase away from actions module; clean function exposure

--- front-end/providers/deck/useDeckMode.jsx
+++ front-end/providers/deck/useDeckMode.jsx
@@ -5,18 +5,15 @@ import { useState, useEffect } from "react";
 export const useDeckMode = () => {
   const [currentMode, setCurrentMode] = useState("default");
 
-  const startPractice = () => setCurrentMode("practicing");
-  const startEditing = () => setCurrentMode("editing");
-  const startInspecting = () => setCurrentMode("inspecting");
-  const setFinished = () => setCurrentMode("finished");
+  const setMode = {
+    practice: () => setCurrentMode("practicing"),
+    edit: () => setCurrentMode("editing"),
+    inspect: () => setCurrentMode("inspecting"),
+    finished: () => setCurrentMode("finished"),
+  };
 
   return {
     currentMode,
-    actions: {
-      startPractice,
-      startEditing,
-      startInspecting,
-      setFinished,
-    },
+    setMode,
   };
 };
