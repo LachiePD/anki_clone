5171e62cc820dd641696bb70a7e21efa72a6a801
lachie <lachiepd@gmail.com>
2026-04-02 00:24:24 +1100

    feature: useCard() hook now consumed by useActiveDeck() context

--- front-end/providers/deck/ActiveDeckProvider.jsx
+++ front-end/providers/deck/ActiveDeckProvider.jsx
@@ -2,18 +2,28 @@
 import { useContext, createContext, useEffect } from "react";
 import { useDeckList, useApi } from "@/providers/index.mjs";
 import { useDeckState } from "./useDeckState.jsx";
+import { useCard } from "./useCard.jsx";
 const ActiveDeckContext = createContext();
 
 export const ActiveDeckProvider = ({ children }) => {
   const deckList = useDeckList(); //strange coupling
   const deck = useDeckState();
+  const card = useCard();
   const api = useApi();
 
   useEffect(() => {
     if (!deck.id) return;
     deck.setMode.inspect();
     fetchCards();
+    if (deckLength !== 0) {
+      updateCard();
+    }
   }, [deck.id]);
+
+  const updateCard = () => {
+    const currentCard = deck.cardList[deck.index];
+    card.setNewCard(currentCard);
+  };
   //maybe check if this is necessary, may be stored in the sidebar anyway
   const fetchCards = async () => {
     const data = await api.card.fetchByDeck(deck.id);
@@ -24,9 +34,19 @@ export const ActiveDeckProvider = ({ children }) => {
     await fetchCards();
   };
 
+  const drawNextCard = () => {
+    deck.increment();
+    if (deck.isFinished()) {
+      deck.finishDeck();
+      return;
+    }
+    updateCard();
+  };
+
   const value = {
     deck,
     refresh,
+    card,
   };
 
   return (
