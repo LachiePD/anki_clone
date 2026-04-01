52ca0b1c56cf3165d72e582c1ecd5b18777f18c3
lachie <lachiepd@gmail.com>
2026-04-02 07:09:13 +1100

    feature: stage/practice now functional

--- front-end/components/stage/views/Practice.jsx
+++ front-end/components/stage/views/Practice.jsx
@@ -1,5 +1,15 @@
 import { useActiveDeck } from "@/providers/index.mjs";
 export const Practice = () => {
-  const { card } = useActiveDeck();
-  return <>'practice'</>;
+  const { card, deck } = useActiveDeck();
+  console.log(card.revealed);
+  return (
+    <>
+      {!card.revealed ? (
+        <> Question: {card.front} </>
+      ) : (
+        <> Answer {card.back}</>
+      )}
+      <br />
+    </>
+  );
 };
