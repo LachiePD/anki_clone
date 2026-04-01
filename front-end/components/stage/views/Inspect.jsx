03e0dd93fc59e3442fbd872af5c217402314d5f3
lachie <lachiepd@gmail.com>
2026-04-01 21:30:54 +1100

    refactor: <Inspect/> is now going to be <Default/>

--- front-end/components/stage/views/Inspect.jsx
+++
@@ -1,7 +0,0 @@
-import { useActiveDeck } from "@/providers/index.mjs";
-
-export const Inspect = () => {
-  const activeDeck = useActiveDeck();
-  const cardCount = activeDeck.cardList.length;
-  return <div>"Number of cards: " {cardCount}</div>;
-};
