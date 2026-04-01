6a8c6be795fa264299fc17fb90fcc7138817978b
lachie <lachiepd@gmail.com>
2026-04-01 21:21:32 +1100

    refactor: changing the methods to reflect variables are now in a more local scope; feat: can now receive length and setId from the context

--- front-end/providers/deck/useDeckState.jsx
+++ front-end/providers/deck/useDeckState.jsx
@@ -8,14 +8,16 @@ export const useDeckState = () => {
   });
   const length = deck.cardList.length;
 
-  const setIndex = (index) => setDeck({ ...deck, index });
   const setCardList = (cardList) => setDeck({ ...deck, cardList });
+  const setIndex = (index) => setDeck((prev) => ({ ...prev, index }));
+  const setId = (id) => setDeck({ ...deck, id });
 
   const setMode = {
     practice: () => setDeck({ ...deck, mode: "practicing" }),
     edit: () => setDeck({ ...deck, mode: "editing" }),
     inspect: () => setDeck({ ...deck, mode: "inspecting" }),
     finished: () => setDeck({ ...deck, mode: "finished" }),
+    default: () => setDeck({ ...deck, mode: "default" }),
   };
   const getCard = () => {
     const index = deck.index;
@@ -31,15 +33,14 @@ export const useDeckState = () => {
 
   const increment = () => {
     const newIndex = 1 + deck.index;
-    deck.setIndex(newIndex);
+    setIndex(newIndex);
   };
-  //note, i think a bug will appear here, the index wont be updated when isFinished runs
+
   const isFinished = () => {
     const index = deck.index;
-    const deckLength = getDeckLength();
-    if (index >= deck.length) {
-      deck.setMode.finished();
-      deck.setIndex(0);
+    if (index >= length) {
+      setMode.finished();
+      setIndex(0);
       return true;
     } else {
       return false;
@@ -49,9 +50,12 @@ export const useDeckState = () => {
   return {
     id: deck.id,
     mode: deck.mode,
+    length,
     setCardList,
     setMode,
     getCard,
+    setId,
+
     drawNextCard, //this should probably be owned by the context
   };
 };
