499e56f4b08c69fd68b1bc4381f05cd3386bb0ed
lachie <lachiepd@gmail.com>
2026-04-11 07:04:36 +1000

    feature: button to set the activeDeck state to default

--- front-end/components/interface/Interface.jsx
+++ front-end/components/interface/Interface.jsx
@@ -2,6 +2,7 @@
 import { Card } from "@/components/ui/Card";
 import { Creator, Practice, Edit, Default, Finished } from "./views/index.mjs";
 import { useActiveDeck } from "@/providers/index.mjs";
+import { Reset } from "./Reset.jsx";
 export const Interface = () => {
   const { deck } = useActiveDeck();
 
@@ -14,6 +15,7 @@ export const Interface = () => {
 
   return (
     <Card className={"flex-row  items-end"}>
+      {deck.mode !== "default" && <Reset />}
       {!deck.id && <Creator />}
       {deck.id && chooseMode[deck.mode]}
     </Card>
