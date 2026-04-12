95e29a2dff75b455198ad29b154f5f46c15eef58
lachie <lachiepd@gmail.com>
2026-04-13 06:49:23 +1000

    chore: removing functionality from <Finished/> as <Reset/>  component can now handle it

--- front-end/components/interface/views/Finished.jsx
+++ front-end/components/interface/views/Finished.jsx
@@ -2,7 +2,5 @@ import { useActiveDeck } from "@/providers/index.mjs";
 export const Finished = () => {
   const { updateDeck } = useActiveDeck();
 
-  return (
-    <button onClick={() => updateDeck({ mode: "default" })}>Go Back</button>
-  );
+  return <></>;
 };
