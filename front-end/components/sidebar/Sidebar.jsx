ceeba4bcfa106f81b7f27f3072e06c49b0d37a4a
lachie <lachiepd@gmail.com>
2026-04-16 07:29:24 +1000

    feat: sidebar now animated, can be opened/closed

--- front-end/components/sidebar/Sidebar.jsx
+++ front-end/components/sidebar/Sidebar.jsx
@@ -1,9 +1,12 @@
 import { NavItem } from "./NavItem.jsx";
 import { Subheader } from "@/components/index.mjs";
 import { useDeckList, useActiveDeck } from "@/providers/index.mjs";
+import { HamburgerBar } from "./HamburgerBar.jsx";
+import { useSidebar } from "./useSidebar";
 export const Sidebar = () => {
   const deckList = useDeckList();
   const { deck: activeDeck, updateDeck } = useActiveDeck();
+  const { visibleRef, toggleVisible, visible } = useSidebar();
 
   const renderDecks = () => {
     return deckList.decks.map((deck, index) => (
@@ -13,9 +16,19 @@ export const Sidebar = () => {
     ));
   };
   return (
-    <nav className={" bg-secondary p-4 min-w-76"}>
-      <Subheader>Deck List</Subheader>
-      <ul>{renderDecks()}</ul>
-    </nav>
+    <>
+
+      <nav
+        className={`
+        fixed top-0 left-0 h-full w-64 bg-secondary p-4
+        transform transition-transform duration-300 ease-in-out
+        ${visible ? "translate-x-0" : "-translate-x-full"}
+      `}
+        ref={visibleRef}
+      >
+        <Subheader>Deck List</Subheader>
+        <ul>{renderDecks()}</ul>
+      </nav>
+    </>
   );
 };
